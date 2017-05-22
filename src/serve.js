f16040010d2230d39f16d85a9929c04fa5b1b908
Montana Rowe <calcnerd256@gmail.com>
2017-05-22 18:32:25 -0500

    static helloworld

--- src/serve.js
+++ src/serve.js
@@ -1,12 +1,38 @@
 // notes on this server should be in ../prose/server.html
 
-require("http").createServer(
- function(q, s){
+var staticHtml = {
+ "/": "./static/index.html"
+}
+
+function respond(q, s){
+ var url = q.url.split("?")[0];
+ if(url in staticHtml){
   s.setHeader("Content-Type", "text/html");
-  s.statusCode = 200;
-  s.end("");
+  return require("fs").readFile(
+   staticHtml[url],
+   function(err, data){
+    if(err){
+     console.log(err);
+     s.setHeader("Content-Type", "text/plain");
+     s.statusCode = 500;
+     return s.end("oops!")
+    }
+    s.setHeader("Content-Type", "text/html");
+    s.statusCode = 200;
+    s.end(data);
+   }
+  );
  }
-).listen(
- 8080,
- console.log.bind(console, "http://localhost:8080")
-);
+ s.setHeader("Content-Type", "text/plain");
+ s.statusCode = 404;
+ s.end("Not Found");
+}
+
+function serve(port){
+ return require("http").createServer(respond).listen(
+  port,
+  console.log.bind(console, "http://localhost:" + (+port))
+ );
+}
+
+serve(8080);
