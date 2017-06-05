b76b9c09b1bdb6aa03aeaa8b5d9df0b9a0b02800
Montana Rowe <calcnerd256@gmail.com>
2017-06-05 08:33:45 -0500

    extracted Promise shim and friends for reuse

---
+++ src/promise.js
@@ -0,0 +1,85 @@
+function Future(){
+ this.done = false;
+ this.listeners = [];
+}
+Future.prototype.listen = function listen(listener){
+ if(this.done) return listener(this.value);
+ this.listeners.push(listener);
+};
+Future.prototype.resolve = function resolve(value){
+ if(this.done) return;
+ this.done = true;
+ this.value = value;
+ return this.listeners.map(
+  function(f){return f(value);}
+ );
+};
+function Maybe(value, error){
+ if(arguments.length > 1) this.error = error;
+ else this.value = value;
+ this["throw"] = function(){
+  throw(this.error);
+ }
+}
+Maybe.prototype["throw"] = function(){
+ if("error" in this)
+  throw(this.error);
+};
+
+var Promise;
+if(!Promise){
+ Promise = function Promise(maker){
+  var future = new Future();
+  function resolve(value){
+   if("then" in value)
+    value.then(
+     function(v){
+      future.resolve(new Maybe(v));
+     },
+     function(e){
+      future.resolve(new Maybe(null, e));
+     }
+    );
+   else
+    future.resolve(new Maybe(value));
+  }
+  function reject(error){
+   future.resolve(new Maybe(null, error));
+  }
+  try{
+   maker(resolve, reject);
+  }
+  catch(e){
+   reject(e);
+  }
+  this.then = function(goodback, badback){
+   return new Promise(
+    function(res, rej){
+     future.listen(
+      function(m){
+       var eim = "error" in m;
+       var pass = eim ? rej : res;
+       var p = eim ? m.error : m.value;
+       var back = eim ? badback : goodback;
+       if(!back) return pass(p);
+       try{
+        return res(back(p));
+       }
+       catch(e){
+        return rej(e);
+       }
+      }
+     );
+    }
+   );
+  };
+ };
+ Promise.resolve = function(value){
+  if("then" in value) return value;
+  return new Promise(
+   function(res){
+    return res(value);
+   }
+  );
+ };
+}
--- src/serve.js
+++ src/serve.js
@@ -24,7 +24,8 @@ var staticFiles = {
   "./static/desktop.js"
  ],
  "/ajax.js": ["application/javascript; charset=utf-8", "./static/ajax.js"],
- "/client.js": ["application/javascript; charset=utf-8", "./static/client.js"]
+ "/client.js": ["application/javascript; charset=utf-8", "./static/client.js"],
+ "/promise.js": ["application/javascript; charset=utf-8", "./promise.js"]
 };
 
 function respondPlain(response, status, body){
@@ -86,91 +87,8 @@ function redirectToGithub(response){
  return respondPlain(response, 302, "redirecting to GitHub")
 }
 
-function Future(){
- this.done = false;
- this.listeners = [];
-}
-Future.prototype.listen = function listen(listener){
- if(this.done) return listener(this.value);
- this.listeners.push(listener);
-};
-Future.prototype.resolve = function resolve(value){
- if(this.done) return;
- this.done = true;
- this.value = value;
- return this.listeners.map(
-  function(f){return f(value);}
- );
-};
-function Maybe(value, error){
- if(arguments.length > 1) this.error = error;
- else this.value = value;
- this["throw"] = function(){
-  throw(this.error);
- }
-}
-Maybe.prototype["throw"] = function(){
- if("error" in this)
-  throw(this.error);
-};
-
 var Promise;
-if(!Promise){
- Promise = function Promise(maker){
-  var future = new Future();
-  function resolve(value){
-   if("then" in value)
-    value.then(
-     function(v){
-      future.resolve(new Maybe(v));
-     },
-     function(e){
-      future.resolve(new Maybe(null, e));
-     }
-    );
-   else
-    future.resolve(new Maybe(value));
-  }
-  function reject(error){
-   future.resolve(new Maybe(null, error));
-  }
-  try{
-   maker(resolve, reject);
-  }
-  catch(e){
-   reject(e);
-  }
-  this.then = function(goodback, badback){
-   return new Promise(
-    function(res, rej){
-     future.listen(
-      function(m){
-       var eim = "error" in m;
-       var pass = eim ? rej : res;
-       var p = eim ? m.error : m.value;
-       var back = eim ? badback : goodback;
-       if(!back) return pass(p);
-       try{
-        return res(back(p));
-       }
-       catch(e){
-        return rej(e);
-       }
-      }
-     );
-    }
-   );
-  };
- };
- Promise.resolve = function(value){
-  if("then" in value) return value;
-  return new Promise(
-   function(res){
-    return res(value);
-   }
-  );
- };
-}
+if(!Promise) Promise = require("./promise").Promise;
 
 function dict(pairs){
  var result = {};
