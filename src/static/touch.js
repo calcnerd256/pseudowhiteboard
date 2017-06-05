6296735c84a865aec33b32a9d83ea258bd7922b9
Montana Rowe <calcnerd256@gmail.com>
2017-06-05 14:46:06 -0500

    extracted Stroke class for touch gestures

--- src/static/touch.js
+++ src/static/touch.js
@@ -1,4 +1,4 @@
-function getViewport(){
+function getBrowserViewport(){
  // thanks https://github.com/ryanve/verge/blob/master/src/verge.js
  var win = null;
  var doc = null;
@@ -10,45 +10,72 @@ function getViewport(){
  y = Math.max(doc["clientHeight"], win["innerHeight"]);
  return {w: x, h: y};
 }
-function line(ctx, xs, ys, xe, ye){
+function drawLine(ctx, xs, ys, xe, ye){
  ctx.beginPath();
  ctx.moveTo(xs, ys);
  ctx.lineTo(xe, ye);
  ctx.stroke();
 }
+
+function Stroke(index, firstTouch, ctx){
+ this.index = index;
+ this.points = [];
+ this.addPoint(firstTouch.screenX, firstTouch.screenY, ctx);
+}
+Stroke.prototype.addPoint = function addPoint(x, y, ctx){
+ this.x = x;
+ this.y = y;
+ this.ctx = ctx;
+ this.points.push([x, y]);
+}
+Stroke.prototype.done = false;
+Stroke.prototype.end = function end(){
+ this.done = true;
+};
+Stroke.prototype.moveTo = function moveTo(nextTouch, ctx){
+ var x = nextTouch.screenX;
+ var y = nextTouch.screenY;
+ drawLine(this.ctx, this.x, this.y, x, y);
+ this.addPoint(x, y, ctx);
+};
+
 function promiseInitCanvas(canv){
- var posn = {};
- function start(touchEvent){
-  touchEvent.preventDefault;
-  [].slice.call(touchEvent.changedTouches).map(
-   function(touch){
-    posn[touch.identifier] = [touch.screenX, touch.screenY];
-   }
-  );
+ var strokeQueue = [];
+ var strokes = {};
+ function beginStroke(touch, ctx){
+  var index = touch.identifier;
+  if(index in strokes) strokes[index].end();
+  var stroke = new Stroke(index, touch, ctx);
+  strokes[index] = stroke;
+  strokeQueue.push(stroke);
+  return stroke;
  }
  return Promise.resolve(canv.getContext("2d")).then(
   function(ctx){
-   var vp = getViewport();
+   var vp = getBrowserViewport();
    canv.width = vp.w - 16;
    canv.height = vp.h - 16;
-   canv.addEventListener("touchstart", start);
 
-   function l(te){
-    te.preventDefault();
-    var strs = [].slice.call(te.touches).map(
-     function(t){
-      var p = posn[t.identifier];
-      line(ctx, p[0], p[1], t.screenX, t.screenY);
-      posn[t.identifier] = [t.screenX, t.screenY];
-      return "(" + t.identifier + ": " + [
-       "x=" + t.screenX,
-       "y=" + t.screenY
-      ].join(" ") + ")";
+   function start(touchEvent){
+    [].slice.call(touchEvent.changedTouches).map(
+     function(touch){
+      var stroke = beginStroke(touch, ctx);
+      touchEvent.preventDefault;
      }
     );
    }
-   canv.addEventListener("touchmove", l);
-   canv.addEventListener("touchend", l);
+   canv.addEventListener("touchstart", start);
+
+   function route(te){
+    te.preventDefault();
+    [].slice.call(te.touches).map(
+     function(t){
+      strokes[t.identifier].moveTo(t, ctx);
+     }
+    );
+   }
+   canv.addEventListener("touchmove", route);
+   canv.addEventListener("touchend", route);
    return ctx;
   }
  );
